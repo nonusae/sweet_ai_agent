cde044efca625a879215712fef5724eff5687fb7
Natthakit Iewprasert <nonusae@gmail.com>
2025-07-08 01:42:16 +0700

    Add genai and run basic function

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from sweet-ai-agent!")
+import os
+from dotenv import load_dotenv
 
+import os
+from google import genai
+from dotenv import load_dotenv
+
+def main():
+    load_dotenv()
+    api_key = os.getenv("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model= 'gemini-2.0-flash-001',
+        contents= "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print(response.text)
+    prompt_token_count = response.usage_metadata.prompt_token_count
+    candidate_token_count = response.usage_metadata.candidates_token_count
+    print(f"Prompt tokens: {prompt_token_count}")
+    print(f"Response tokens: {candidate_token_count}")
 
 if __name__ == "__main__":
     main()
