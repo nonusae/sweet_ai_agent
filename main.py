7315738baaa5448b2350e472f95a694223629f09
Natthakit Iewprasert <nonusae@gmail.com>
2025-07-08 20:11:37 +0700

    Refactor some code and add verbose condition check

--- main.py
+++ main.py
@@ -1,39 +1,48 @@
-import os
 import sys
-from dotenv import load_dotenv
-
 import os
 from google import genai
-from dotenv import load_dotenv
 from google.genai import types
+from dotenv import load_dotenv
+
 
 def main():
-    if len(sys.argv) == 1:
-        raise Exception("No arguments provided")
-    else:
-        prompt = sys.argv[1]
-        messages = [
-            types.Content(
-                role="user",
-                parts=[
-                    types.Part(
-                        text=prompt
-                    )
-                ]
-            )
-        ]
-        load_dotenv()
-        api_key = os.getenv("GEMINI_API_KEY")
-        client = genai.Client(api_key=api_key)
-        response = client.models.generate_content(
-            model= 'gemini-2.0-flash-001',
-            contents= messages)
+    load_dotenv()
 
-        print(response.text)
-        prompt_token_count = response.usage_metadata.prompt_token_count
-        candidate_token_count = response.usage_metadata.candidates_token_count
-        print(f"Prompt tokens: {prompt_token_count}")
-        print(f"Response tokens: {candidate_token_count}")
+    verbose = "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
+
 
 if __name__ == "__main__":
     main()
