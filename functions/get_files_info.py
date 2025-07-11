00623ae5c493da6655e1ee252b7c1919d384edb8
Natthakit Iewprasert <nonusae@gmail.com>
2025-07-11 22:42:16 +0700

    ADd write file functions

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,33 +1,32 @@
 import os
 
 def get_files_info(working_directory, directory=None):
-  try:
-    abs_working_dir = os.path.abspath(working_directory)
-    full_path = os.path.abspath(os.path.join(working_directory, directory))
+    try:
+      abs_working_dir = os.path.abspath(working_directory)
+      full_path = os.path.abspath(os.path.join(working_directory, directory))
 
-    if not full_path.startswith(abs_working_dir):
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+      if not full_path.startswith(abs_working_dir):
+          return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    if not os.path.isdir(full_path):
-        return f'Error: "{directory}" is not a directory'
+      if not os.path.isdir(full_path):
+          return f'Error: "{directory}" is not a directory'
 
-    result = []
-    for file in os.listdir(full_path):
-      file_path = os.path.join(full_path, file)
-      if os.path.isdir(file_path):
-        size = get_dir_size(file_path)
-        result.append(f'- {file}: file_size={size} bytes, is_dir=True')
-      else:
-        result.append(f'- {file}: file_size={os.path.getsize(file_path)} bytes, is_dir=False')
-    return '\n'.join(result)
-  except Exception as e:
-    return f'Error: {e}'
+      result = []
+      for file in os.listdir(full_path):
+        file_path = os.path.join(full_path, file)
+        if os.path.isdir(file_path):
+          size = get_dir_size(file_path)
+          result.append(f'- {file}: file_size={size} bytes, is_dir=True')
+        else:
+          result.append(f'- {file}: file_size={os.path.getsize(file_path)} bytes, is_dir=False')
+      return '\n'.join(result)
+    except Exception as e:
+      return f'Error: {e}'
 
 def get_file_content(working_directory, file_path):
     try:
       abs_working_dir = os.path.abspath(working_directory)
       full_path = os.path.abspath(os.path.join(working_directory, file_path))
-      print(full_path)
       if not os.path.commonpath([full_path, abs_working_dir]) == abs_working_dir:
         print(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
 
@@ -45,6 +44,29 @@ def get_file_content(working_directory, file_path):
     except Exception as e:
       return f'Error: {e} '
 
+def write_file(working_directory, file_path, content):
+  try:
+    abs_working_dir = os.path.abspath(working_directory)
+    full_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not os.path.commonpath([full_path, abs_working_dir]) == abs_working_dir:
+      print(f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory')
+
+    if not os.path.exists(full_path):
+      #create the file
+      f = open(full_path, 'x')
+      f.write(content)
+    else:
+      #overwrite the file
+      f = open(full_path, 'w')
+      f.write(content)
+      f.close()
+
+    return (f'Successfully wrote to "{file_path}" ({len(content)} characters written)')
+  except Exception as e:
+    return f'Error: {e}'
+
+
+
 def get_dir_size(dir_path):
   total_size = 0
   for file in os.listdir(dir_path):
@@ -54,4 +76,6 @@ def get_dir_size(dir_path):
     else:
       total_size += os.path.getsize(file_path)
   return total_size
+
+
 
--- tests.py
+++ tests.py
@@ -1,32 +1,38 @@
 import unittest
-from functions.get_files_info import get_files_info, get_file_content
+from functions.get_files_info import get_files_info, get_file_content, write_file
 
 class TestGetFilesInfo(unittest.TestCase):
-    def test_get_files_info_calculator(self):
-        result = get_files_info('calculator', '.')
-        print("Result for current directory:")
-        print(result)
+    # def test_get_files_info_calculator(self):
+    #     result = get_files_info('calculator', '.')
+    #     print("Result for current directory:")
+    #     print(result)
 
-    def test_get_files_info_calculator_pkg(self):
-        result = get_files_info('calculator', 'pkg')
-        print("Result for 'pkg' directory:")
-        print(result)
+    # def test_get_files_info_calculator_pkg(self):
+    #     result = get_files_info('calculator', 'pkg')
+    #     print("Result for 'pkg' directory:")
+    #     print(result)
 
-    def test_get_files_info_calculator_invalid(self):
-        result = get_files_info('calculator', '/bin')
-        print("Result for '/bin' directory:")
-        print(result)
+    # def test_get_files_info_calculator_invalid(self):
+    #     result = get_files_info('calculator', '/bin')
+    #     print("Result for '/bin' directory:")
+    #     print(result)
 
-    def test_get_files_info_calculator_invalid_outside_dir(self):
-        result = get_files_info('calculator', '../')
-        print("Result for '../' directory:")
-        print(result)
+    # def test_get_files_info_calculator_invalid_outside_dir(self):
+    #     result = get_files_info('calculator', '../')
+    #     print("Result for '../' directory:")
+    #     print(result)
 
-    def test_get_file_content_calculator_main(self):
-        print(get_file_content("calculator", "lorem.txt"))
-        print(get_file_content('calculator', 'main.py'))
-        print(get_file_content('calculator', 'pkg/calculator.py'))
-        print(get_file_content('calculator', '/bin/cat'))
+    # def test_get_file_content_calculator_main(self):
+    #     print(get_file_content("calculator", "lorem.txt"))
+    #     print(get_file_content('calculator', 'main.py'))
+    #     print(get_file_content('calculator', 'pkg/calculator.py'))
+    #     print(get_file_content('calculator', '/bin/cat'))
+
+    def test_write_file_calculator_main(self):
+        print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+        print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+        print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
